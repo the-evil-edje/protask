7a4fb1e62f93b7dcc88e7d7c97a09e3ab8418700
ImHuntd <imhuntd@gmail.com>
2018-06-04 09:14:40 +0200

    Unity Game

---
+++ Protask Project/Assets/scripts/respawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour {
+
+    public float threshold;
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < threshold)
+            transform.position = new Vector3(0, 0, 0);
+    }
+}
+
